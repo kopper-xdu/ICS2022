5c8919e4b1941d75530c913ea7958829be300064
tracer-ics2022 <tracer@njuics.org>
2023-07-07 17:23:15 +0800

    >  compile NEMU 211220000 张三 Linux kopper-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:15 up  2:07,  1 user,  load average: 0.39, 0.56, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = inw(RTC_ADDR);
+  uptime->us = inw(RTC_ADDR + 4);
   printf("uptime->us = %d\n", uptime->us);
 }
 
