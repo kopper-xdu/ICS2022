715a81538d550670591cd0d8bc7ab084135a81ac
tracer-ics2022 <tracer@njuics.org>
2023-07-07 20:26:07 +0800

    >  compile NEMU 211220000 张三 Linux kopper-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:07 up  3:27,  1 user,  load average: 2.10, 1.19, 0.98

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -60,14 +60,13 @@ static long load_img()
 
 	fseek(fp, 0, SEEK_END);
 	long size = ftell(fp);
-
+printf("---------------1");
 	Log("The image is %s, size = %ld", img_file, size);
 
 	fseek(fp, 0, SEEK_SET);
 	int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
 	assert(ret == 1);
 
-	printf("---------------1");
 
 	fclose(fp);
 
