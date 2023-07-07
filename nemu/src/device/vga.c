ee31f05755e747f21584916618a45a92a130863e
tracer-ics2022 <tracer@njuics.org>
2023-07-07 20:09:07 +0800

    >  compile NEMU 211220000 张三 Linux kopper-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:07 up  3:10,  1 user,  load average: 2.15, 1.27, 1.07

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -70,10 +70,17 @@ static inline void update_screen() {
 #endif
 #endif
 
+
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  uint32_t sync = vgactl_port_base[1];
+  if (sync) {
+    update_screen();
+    vgactl_port_base[1] = 0;
+  }
 }
+
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
